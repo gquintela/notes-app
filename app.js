f8c036256bcf674a81906a9b02791c7bc4dd2d09
gquintela <gquintela@dc.uba.ar>
2020-03-24 18:57:47 -0300

    added addNote, removeNote, list, reset, read functionalities

--- app.js
+++ app.js
@@ -1,6 +1,101 @@
-const getNotes = require("./notes.js");
+const notes = require("./notes.js");
 const chalk = require("chalk");
+const yargs = require("yargs");
+const fs = require("fs");
+const clear = require("clear");
 
-console.log(chalk.bold.green.bgBlue("Success!"));
+yargs.version("1.1.0");
 
-console.log(process.argv);
+// add command
+yargs.command({
+  command: "add",
+  describe: "add a new note (--title and --body are required.)",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "short description of the item",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: addNewNote
+});
+
+//remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note (--title is required.)",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: removeNote
+});
+
+//read command
+yargs.command({
+  command: "read",
+  describe: "read a note (--title is required.)",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: readNote
+});
+
+//list command
+yargs.command({
+  command: "list",
+  describe: "List all the notes titles",
+  handler: listNotes
+});
+
+//reset all notes command
+yargs.command({
+  command: "reset",
+  describe: "Reset the entire notes, this will ERASE ALL NOTES.",
+  handler: eraseAll
+});
+
+// handler functions
+
+function addNewNote(argv) {
+  clear();
+  console.log("Adding new note...");
+  notes.addNote(argv["title"], argv["body"]);
+}
+
+function removeNote(argv) {
+  clear();
+  console.log("Removing note...");
+  notes.removeNote(argv["title"]);
+}
+
+function readNote(argv) {
+  clear();
+  notes.readNote(argv["title"]);
+}
+
+function listNotes() {
+  clear();
+  console.log("listing all notes!\n");
+  notes.listNotes();
+}
+
+function eraseAll() {
+  clear();
+  console.log("Deleting all content...");
+  notes.deleteAllNotes();
+}
+
+yargs.parse();
--- notes.js
+++ notes.js
@@ -1,5 +1,89 @@
-function getNotes() {
-  return "Your notes...";
-}
+const fs = require("fs");
+const chalk = require("chalk");
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+  const notes = loadNotes();
+  if (notes[title] === undefined) {
+    notes[title] = body;
+    console.log(chalk.green.bold.inverse("Note added!"));
+    saveNotes(notes);
+  } else {
+    console.log(
+      chalk.red.bold.inverse(
+        "Error: the note '",
+        title,
+        "' exists, please delete it before adding a new body. \nAdding note aborted."
+      )
+    );
+  }
+};
+
+const removeNote = function(title) {
+  const notes = loadNotes();
+  if (notes[title] === undefined) {
+    console.log(
+      chalk.red.bold.inverse("There is not a note with the title:", title)
+    );
+    console.log(chalk.red.bold.inverse("removing aborted."));
+  } else {
+    let newNotes = {};
+    if (Object.keys(notes).length > 1) {
+      newNotes = notes;
+      delete newNotes[title];
+    }
+    saveNotes(newNotes);
+    console.log(chalk.green.bold.inverse(title, "deleted."));
+  }
+};
+
+const listNotes = function() {
+  const notes = loadNotes();
+  const notesArray = Object.keys(notes);
+  notesArray.forEach(element => {
+    console.log("_", element);
+  });
+};
+
+const readNote = function(title) {
+  notes = loadNotes();
+  if (notes[title] === undefined) {
+    console.log(chalk.red.bold("note", title, "didn't found."));
+  } else {
+    console.log(chalk.green.bold.inverse(title, ":"));
+    console.log(chalk.green.bold.inverse(notes[title]));
+  }
+};
+
+const deleteAllNotes = function() {
+  notes = {};
+  saveNotes(notes);
+  console.log(chalk.green.bold.inverse("All notes deleted."));
+};
+
+// aux functions  //////////////////////////////////////////
+const saveNotes = function(notes) {
+  fs.writeFileSync("notes.json", JSON.stringify(notes));
+};
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    return JSON.parse(dataBuffer.toString());
+  } catch (e) {
+    console.log("there is no 'notes.json' file.\nCreating \"notes.json\"... ");
+    fs.writeFileSync("notes.json", "{}");
+    console.log(chalk.green.bold.inverse('"notes.json" created.'));
+    return {};
+  }
+};
+
+//exports /////
+
+module.exports = {
+  // getNotes: getNotes,
+  addNote: addNote,
+  deleteAllNotes: deleteAllNotes,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote
+};
